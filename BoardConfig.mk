cfec8d19368bfe6fbbf03a8d4eaf2000270eb3fc
mandfx <mandfx@qq.com>
2014-12-07 21:26:41 +0800

    add initc8816

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,6 +106,12 @@ ifneq ($(MK_VERSION),)
     BOARD_HARDWARE_CLASS := $(LOCAL_PATH)/mkhw/
 endif
 
+
+# Init
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_c8816.c
+
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
